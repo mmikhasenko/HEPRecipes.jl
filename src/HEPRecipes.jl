1b8cb5c5ee8a8728f32ce985d7849bab967a9c67
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2020-09-01 21:31:43 +0200

    added QuadGK

--- src/HEPRecipes.jl
+++ src/HEPRecipes.jl
@@ -1,13 +1,14 @@
 module HEPRecipes
 
 using RecipesBase
+using QuadGK
 
 export weightedHistogram
 export entries, contents, bincenters, nbins
 export yerror, xerror
 include("weightedHistogram.jl")
 
+export pull
 include("recipe.jl")
-
 
 end # module
